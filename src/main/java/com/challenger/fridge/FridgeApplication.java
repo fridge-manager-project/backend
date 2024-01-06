e82432126baa1193ce6654b345fb49e87f57bac5
Hyeon Seok <tlatms8919@gmail.com>
2024-01-06 11:22:41 +0900

    test

--- src/main/java/com/challenger/fridge/FridgeApplication.java
+++ src/main/java/com/challenger/fridge/FridgeApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FridgeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("\"master\" = " + "master");
 		SpringApplication.run(FridgeApplication.class, args);
 	}
-
+	
 }
