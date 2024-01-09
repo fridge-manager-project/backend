0fcbdf368fd292b42516befc2944326fc8ae9f81
Hyeon Seok <tlatms8919@gmail.com>
2024-01-09 21:04:01 +0900

    Repository어노테이션 제거

--- src/main/java/com/challenger/fridge/repository/CategoryRepository.java
+++ src/main/java/com/challenger/fridge/repository/CategoryRepository.java
@@ -3,9 +3,9 @@ package com.challenger.fridge.repository;
 
 import com.challenger.fridge.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByCategoryName(String categoryName);
 
--- src/main/java/com/challenger/fridge/repository/ItemRepository.java
+++ src/main/java/com/challenger/fridge/repository/ItemRepository.java
@@ -3,9 +3,9 @@ package com.challenger.fridge.repository;
 
 import com.challenger.fridge.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
 }
