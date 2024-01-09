252488847d67ec23607d4ca3cacf2b8916c48eb7
Hyeon Seok <tlatms8919@gmail.com>
2024-01-09 20:27:41 +0900

    파일 데이터 불러와서 DB에 넣기

---
+++ src/main/java/com/challenger/fridge/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.challenger.fridge.repository;
+
+
+import com.challenger.fridge.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByCategoryName(String categoryName);
+
+    Category findByCategoryNameAndParentCategory(String categoryName,Category category);
+    Boolean existsByCategoryName(String categoryName);
+}
