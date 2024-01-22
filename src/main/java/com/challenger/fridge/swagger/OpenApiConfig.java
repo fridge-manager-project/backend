aece04c4bb8993a63dda9d9a35db325664009ba4
ostar11 <cjw5877@gmail.com>
2024-01-22 17:55:27 +0900

    Feat: Swagger accessToken 설정 추가

--- src/main/java/com/challenger/fridge/swagger/OpenApiConfig.java
+++ src/main/java/com/challenger/fridge/swagger/OpenApiConfig.java
@@ -1,5 +1,8 @@
 package com.challenger.fridge.swagger;
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,14 +12,23 @@ import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class OpenApiConfig {
+    private static final String SECURITY_SCHEME_NAME = "authorization";
+
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
                 .title("냉글벙글 API")
                 .version("1.0")
                 .description("API명세 입니다.");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .info(info);
     }
 }
