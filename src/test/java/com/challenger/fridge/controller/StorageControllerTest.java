13e455b861a779ebe41ea2671da49a16224763f6
Hyeon Seok <tlatms8919@gmail.com>
2024-01-23 15:18:36 +0900

    Feat: 보관소 추가 컨트롤러 테스트 완성 후 버그 발생

---
+++ src/test/java/com/challenger/fridge/controller/StorageControllerTest.java
@@ -0,0 +1,76 @@
+package com.challenger.fridge.controller;
+
+
+import com.challenger.fridge.common.StorageMethod;
+import com.challenger.fridge.dto.ApiResponse;
+import com.challenger.fridge.dto.storage.request.StorageRequest;
+import com.challenger.fridge.service.StorageService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(StorageController.class)
+class StorageControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    StorageService storageService;
+
+    @Test
+    @DisplayName("POST 보관소 추가 컨트롤러 동작")
+    public void 보관소추가컨트롤러() throws Exception {
+        ApiResponse apiSuccessResponse = createApiSuccessResponse(null);
+        String userEmail = "123@naver.com";
+        StorageRequest storageRequest = new StorageRequest("냉장고", StorageMethod.FRIDGE);
+        given(storageService.saveStorage(storageRequest, userEmail)).willReturn(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/storage")
+                        .content(asJsonString(createStorageJson()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(asJsonString(apiSuccessResponse)));
+    }
+
+    public String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JSONObject createStorageJson() {
+        Map<String, String> map = new HashMap<>();
+        map.put("storageName", "냉장고");
+        map.put("storageMethod", "FRIDGE");
+        JSONObject jsonObject = new JSONObject(map);
+        return jsonObject;
+    }
+
+    private ApiResponse createApiErrorResponse(String message) {
+        ApiResponse apiResponse = ApiResponse.error(message);
+        return apiResponse;
+    }
+
+    private ApiResponse createApiSuccessResponse(Object obj) {
+        return ApiResponse.success(obj);
+    }
+}
+
+
