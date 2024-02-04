025a56592e12d596349fc0ee24761eb8d71e7e3a
ostar11 <cjw5877@gmail.com>
2024-02-04 14:23:45 +0900

    Feat: 로그인 성공시 리턴값을 Header에 담기
    AccessToken은 AUTHORIZATION, RefreshToken은 Cookie에 저장 후 리턴

--- src/main/java/com/challenger/fridge/controller/SignController.java
+++ src/main/java/com/challenger/fridge/controller/SignController.java
@@ -2,12 +2,17 @@ package com.challenger.fridge.controller;
 
 import com.challenger.fridge.dto.ApiResponse;
 import com.challenger.fridge.dto.sign.SignInRequest;
+import com.challenger.fridge.dto.sign.SignInResponse;
 import com.challenger.fridge.dto.sign.SignUpRequest;
 import com.challenger.fridge.service.SignService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class SignController {
 
+    private final long COOKIE_EXPIRATION = 7776000; // 90일
     private final SignService signService;
 
     @Operation(summary = "회원 이메일 중복 체크")
@@ -41,7 +47,19 @@ public class SignController {
 
     @Operation(summary = "로그인")
     @PostMapping("/sign-in")
-    public ApiResponse signUp(@RequestBody SignInRequest request) {
-        return ApiResponse.success(signService.signIn(request));
+    public ResponseEntity<?> signUp(@RequestBody SignInRequest request) {
+//        return ApiResponse.success(signService.signIn(request));
+        SignInResponse response = signService.signIn(request);
+
+        HttpCookie httpCookie = ResponseCookie.from("refresh-token", response.getTokenInfo().getRefreshToken())
+                .maxAge(COOKIE_EXPIRATION)
+                .httpOnly(true)
+                .secure(true)
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, httpCookie.toString())
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + response.getTokenInfo().getAccessToken())
+                .build();
     }
 }
