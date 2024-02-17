cce117bc17fbf1bad45ce307ee4549583648484a
ostar11 <cjw5877@gmail.com>
2024-02-17 22:44:44 +0900

    Refactor: CartRepository 사용하지 않는 메서드 삭제

--- src/main/java/com/challenger/fridge/repository/CartRepository.java
+++ src/main/java/com/challenger/fridge/repository/CartRepository.java
@@ -12,10 +12,4 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     @Query("select c from Cart c join fetch c.member where c.member.email = :email")
     Optional<Cart> findByMemberEmail(@Param("email") String email);
 
-    @Query("select distinct c from Cart c "
-            + " join fetch c.member m"
-            + " join fetch c.cartItemList ci"
-            + " join fetch ci.item i"
-            + " where m.email = :email")
-    List<Cart> findItemsByEmail(@Param("email") String email);
 }
