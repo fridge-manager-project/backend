98893f5b76c825f2930c2ce37b9f6f5eda8f26db
ostar11 <cjw5877@gmail.com>
2024-03-07 16:51:29 +0900

    Chore: item정보 조회 시 Cache 적용

--- src/main/java/com/challenger/fridge/service/ItemService.java
+++ src/main/java/com/challenger/fridge/service/ItemService.java
@@ -6,9 +6,12 @@ import com.challenger.fridge.repository.ItemRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
@@ -16,7 +19,9 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    @Cacheable(key = "'all'")
     public List<ItemInfoResponse> itemInfo() {
+        log.info("상품 전체 데이터 조회");
         List<Item> items = itemRepository.findAllWithCategory();
         return items.stream()
                 .map(ItemInfoResponse::new)
