26a2bc6a8134d9501d6d99f886cfcf4ab5f1726f
ostar11 <cjw5877@gmail.com>
2024-03-12 19:26:55 +0900

    Refactor: ItemCountRequest 기본 생성자 추가

--- src/main/java/com/challenger/fridge/dto/cart/ItemCountRequest.java
+++ src/main/java/com/challenger/fridge/dto/cart/ItemCountRequest.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ItemCountRequest {
 
