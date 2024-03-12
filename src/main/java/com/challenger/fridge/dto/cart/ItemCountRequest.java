6fc5b95b12cdbe0310aed93b2a0b8c87704b4217
ostar11 <cjw5877@gmail.com>
2024-03-12 19:11:44 +0900

    Refactor: 장바구니 상품 수량 조절 API 수정
    ItemCountRequest 에 @Data 사용

--- src/main/java/com/challenger/fridge/dto/cart/ItemCountRequest.java
+++ src/main/java/com/challenger/fridge/dto/cart/ItemCountRequest.java
@@ -3,9 +3,10 @@ package com.challenger.fridge.dto.cart;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 
-@Getter
+@Data
 @AllArgsConstructor
 public class ItemCountRequest {
 
