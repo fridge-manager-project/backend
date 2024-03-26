f6cf8c2400fd31dd08d5ecc6f30a6c412048edee
ostar11 <cjw5877@gmail.com>
2024-03-26 18:28:16 +0900

    Refactor: 역질렬화 오류로 인해 CartItemMoveRequest 빈 생성자 추가

--- src/main/java/com/challenger/fridge/dto/cart/CartItemMoveRequest.java
+++ src/main/java/com/challenger/fridge/dto/cart/CartItemMoveRequest.java
@@ -3,9 +3,11 @@ package com.challenger.fridge.dto.cart;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CartItemMoveRequest {
     private Long boxId;
 //    private List<CartItemRequest> cartItemRequests;
